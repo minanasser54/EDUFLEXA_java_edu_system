50dc4c7e5bb30a445820942d62814719657e19f5
Mina_Nasser <mina.nasser54@gmail.com>
2024-04-17 16:00:08 +0200

    removed Utilities

--- src/App.java
+++ src/App.java
@@ -1,18 +1,12 @@
 import accounts.Mentor;
 import accounts.Student;
-import accounts.Utilities;
 
 public class App {
     public static void main(String[] args) throws Exception {
-    //create mentors objects + DataBase
-        //Mentor mina = Mentor.createMentor(0, "mina", "mina@gmail.com", "123", "male");
-        //Mentor nasser = Mentor.createMentorDB(1, "nasser", "nasser@gmail.com", "123", "male");
-    //parsing single and multiple Mentors
-        //Mentor tm[]=Utilities.parseMentors();
-        //System.out.println(tm[1].toString());
-        //Mentor here = Utilities.parseMentor(4);
-        //System.out.println(here.toString());
-        //Mentor here =Utilities.editMentor(1, null, null, null, null);
+        Mentor nasser = Mentor.createMentor(1, "nasser", "nasser@gmail.com", "123", "male");
+        Mentor mina = Mentor.createMentor(0, "mina", "mina@gmail.com", "123", "male");
+        System.out.println(mina.toString());
+        System.out.println(nasser.toString());
         
     }
 }
--- src/accounts/Mentor.java
+++ src/accounts/Mentor.java
@@ -5,11 +5,6 @@ public class Mentor extends User {
     public Mentor(int id, String username, String email, String password, String gender) {
         super(id, username, email, password, gender,"MENTOR");
     }
-    public static Mentor createMentorDB(int id, String username, String email, String password, String gender){
-        Mentor mentor = new Mentor(id, username, email, password, gender);
-        Utilities.addMentorDB(mentor);
-        return mentor;
-    }
     public static Mentor createMentor(int id, String username, String email, String password, String gender){
         Mentor mentor = new Mentor(id, username, email, password, gender);
         return mentor;
