7e62c5e23056bb01df776112c31d527edb4fc5f9
Zyad Magdy <zyadmagdy32@gmail.com>
2024-04-17 15:12:22 +0200

    Second Change

--- src/zyad.java
+++ src/zyad.java
@@ -2,5 +2,9 @@ class Zyad{
     public static void main (String args[])
     {
         System.out.println("Zyad Magdy");
+        System.out.println("Second");
+        System.out.println("Third");
+        System.out.println("Zyad Magdy" + 123*23);
+
     }
 }
