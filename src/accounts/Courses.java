9a6fc6526c3f4ac7425762e1effd5862edd39153
zyadmagdy11 <zyadmagdy32@gmail.com.com>
2024-04-26 14:58:36 +0300

    Course Class

--- src/accounts/Courses.java
+++ src/accounts/Courses.java
@@ -7,10 +7,10 @@ public interface Courses
 {
     ArrayList<String> title             = new ArrayList<String>();
     ArrayList<Integer> code             = new ArrayList<Integer>();
-    ArrayList<Integer> price              = new ArrayList<Integer>();
+    ArrayList<Integer> price            = new ArrayList<Integer>();
     ArrayList<Integer> week_duration    = new ArrayList<Integer>();
     ArrayList<String> Outcomes          = new ArrayList<String>();
-    ArrayList<Integer> no_quize = new ArrayList<Integer>();
+    ArrayList<Integer> no_quize         = new ArrayList<Integer>();
 
     
     static String[][] Chapters = new String[100][20];                  // suppose maximum courses 100, each course has 20 Chapter
